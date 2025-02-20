1ef9ad8f559d635fa0d70c9e171256e2a248ce1a
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-02-20 12:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ DELETE_AFTER = int(os.environ.get("DELETE_AFTER", 60)) #seconds
 NOTIFICATION_TIME = int(os.environ.get('NOTIFICATION_TIME', 60)) #seconds
 AUTO_DELETE = os.environ.get("AUTO_DELETE", True) #ON/OFF
 GET_AGAIN = os.environ.get("GET_AGAIN", False) #ON/OFF
-DELETE_INFORM = os.environ.get("INFORM" , "Successfully DELETED !! For Video Available Here See https://t.me/+rmz5f_q49qk5ZmZl")
+DELETE_INFORM = os.environ.get("INFORM" , "Successfully DELETED !!\n\n Request Again Below Group 🔍\nhttps://t.me/+OXAtR8FYZ_9iMDA1\nhttps://t.me/+xgNhUycdXYA0NTg1\nhttps://t.me/+SR1z9fmWe4UxNDM1")
 NOTIFICATION = os.environ.get("NOTIFICATION" ,"🥵 No link ❌ no ads ❌ Direct video ✅\n\n⛔️ Kannada sex video\n⛔️ Nudi\n⛔️ Oyo\n⛔️ Hidden Cam\n⛔️ Rap video\n⛔️ Brzzer\n⛔️ Only fan\n⛔️ Celebrity sex\n⛔️ Animal Sex\n⛔️ Girls lesbian\n⛔️ Desi video\n⛔️ She male\n⛔️ Savitha Bhabhi \n⛔️ Comic sex video\n⛔️ African girl sex \n⛔️ Sex web series\n\n👆🏻 Above all direct video 👆🏻\n\nJust :- 250₹ (one-time payment, lifetime free)\n\nTo Buy, msg :- @msgmetobuy\n\n✅ All videos are uploaded in different channels with forwarding allowed ✅")
 GET_INFORM = os.environ.get("GET_INFORM" ,"File was deleted after {DELETE_AFTER} seconds. Use the button below to GET FILE AGAIN.")
 
@@ -63,7 +63,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002380159168"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "https://t.me/+p-aHmJNvBNszODM1\n\nhttps://t.me/+jc7Wwu1pxMc1MTFl")
+START_MSG = os.environ.get("START_MESSAGE", "Request Your Favorite Movie Below Group 🔍\n\nhttps://t.me/+OXAtR8FYZ_9iMDA1\nhttps://t.me/+xgNhUycdXYA0NTg1\nhttps://t.me/+SR1z9fmWe4UxNDM1")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6643562770 6643562770 6643562770").split()):
